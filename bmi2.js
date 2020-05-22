98bd7aa1d4785242044b33a6e0847c582663b783
Aravind Kumar Jagannathan <Aravindkumarj@users.noreply.github.com>
2020-05-22 00:01:22 -0500

    Add files via upload

---
+++ bmi2.js
@@ -0,0 +1,24 @@
+var mark = {
+    full_name: "Mark Waugh",
+    mass: 200,
+    height: 5.9,
+    bmiCalc: function(){
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;     
+    }
+};
+
+var john = {
+    full_name: "John Dolan",
+    mass: 200,
+    height: 5.16,
+    bmiCalc: function(){
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;     
+    }
+};
+
+mark.bmiCalc();
+john.bmiCalc();
+
+console.log((mark.bmi > john.bmi)? "Mark's BMI is greater": ((john.bmi > mark.bmi)? "John's BMI is greater" : "Both got same BMI" ));
